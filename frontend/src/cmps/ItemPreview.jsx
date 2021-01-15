0f6ff28ca85bb01c5377fb6855c2e0af2fe61116
Yairus * <itzhakco@gmail.com>
2021-01-15 15:05:53 +0200

    apppriviw

--- frontend/src/cmps/ItemPreview.jsx
+++ frontend/src/cmps/ItemPreview.jsx
@@ -2,7 +2,7 @@ import '../assets/imgs/01.jpg';
 import '../assets/imgs/02.jpg';
 import '../assets/imgs/03.jpg';
 import  '../assets/imgs/04.jpg';
-import '../assets/imgs/05.jpg';
+import x from '../assets/imgs/01.jpg';
 import Button from '@material-ui/core/Button';
 export function ItemPreview({ item ,  onRemoveItem}) {
     return (
@@ -10,7 +10,7 @@ export function ItemPreview({ item ,  onRemoveItem}) {
         <div>
        <ul className="clean-list">
            <li>{item.title}</li>
-           <img src={item.imgUrl} alt="" />
+           <img src={x} alt="" />
            <li>{item.description}</li>
            <li>{item.price}</li>
            <li>{item.seller.fullname}</li>
