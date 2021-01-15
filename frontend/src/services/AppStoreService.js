5abf771a821dc6715829e986a6dff0d144c19b5d
unknown <inbalazmon@gmail.com>
2021-01-15 08:13:32 +0200

    no changes

--- frontend/src/services/AppStoreService.js
+++ frontend/src/services/AppStoreService.js
@@ -120,13 +120,8 @@ var gItems = [
   }
 ]
 function query() {
-<<<<<<< HEAD
 //   return Promise.resolve(gItems);
 return gItems
-=======
-  // return Promise.resolve(gItems);
-  return gItems;
->>>>>>> a5487a04d496f59fcb4029974740c4594a657ccf
 }
 
 function remove(itemId) {
