3fc3694e07b85b4ccd65fda316f5abe3d9235585
Nadav Mozes <numad@live.com>
2021-01-17 08:24:24 +0200

    more styling

--- frontend/src/cmps/ItemPreview.jsx
+++ frontend/src/cmps/ItemPreview.jsx
@@ -1,20 +1,27 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import x from '../assets/imgs/01.jpg';
+// import x from '../assets/imgs/01.jpg';
 import Button from '@material-ui/core/Button';
-export function ItemPreview({ item ,  onRemoveItem}) {
+export function ItemPreview({ item, onRemoveItem, onBuy }) {
+
     return (
 
         <div>
-       <ul className="clean-list">
-           <li>{item.title}</li>
-           <img src={x} alt="" />
-           <li>Description: {item.description}</li>
-           <li>Price: {item.price}</li>
-           <li>Artist: {item.seller.fullname}</li>
-       </ul>
-       <Button onClick={() => { onRemoveItem(item._id) }}>Delete</Button>
-       <Button><Link to={`/item/${item._id}`}>Details</Link></Button>
+            <ul className="clean-list">
+                <li>{item.title}</li>
+                <img src={item.imgUrl} alt="" />
+                <li>Description: {item.description}</li>
+                <li>Price: {item.price}</li>
+                <li>Artist: {item.seller.fullname}</li>
+            </ul>
+            <Button onClick={() => { onRemoveItem(item._id) }}>Delete</Button>
+            <Button><Link to={`/item/${item._id}`}>Details</Link></Button>
+            <li key={item._id}>
+                {item.name} - ${item.price}
+                <button onClick={() => {
+                    this.onBuy(item)
+                }}>Buy</button>
+            </li>
         </div>
     )
 }
