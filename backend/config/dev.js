6bba0d94fab919f536cfbfe48b0308be2dc31cd5
YairusCO <itzhakco@gmail.com>
2021-04-04 13:15:52 +0300

    removed node modules from project

--- backend/config/dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  // 'dbURL': 'mongodb://localhost:27017',
-  'dbURL': 'mongodb+srv://nadavm:q1w2e3r4@nadavm.qkzk9.mongodb.net/itemDB?retryWrites=true&w=majority',
-}
--- backend/config/prod.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  'dbURL': 'mongodb+srv://nadavm:q1w2e3r4@nadavm.qkzk9.mongodb.net/itemDB?retryWrites=true&w=majority',
-}
