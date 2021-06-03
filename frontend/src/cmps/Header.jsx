4dc1af792a2f4f6089bd1a445e762b2573651d37
YairusCO <itzhakco@gmail.com>
2021-06-03 16:43:30 +0300

    fix hamburger

--- frontend/src/cmps/Header.jsx
+++ frontend/src/cmps/Header.jsx
@@ -6,42 +6,24 @@ import Menu from '../assets/imgs/menu.svg';
 import { ItemFilter } from '../cmps/ItemFilter'
 
 class _Header extends Component {
-    // handleInput = ({ target: { name, value, type } }) => {
-    //     const modValue = (type === 'number') ? parseInt(value) : value
-    //     this.setState(prevState => {
-    //       return {
-    //         filterBy: {
-    //           ...prevState.filterBy,
-    //           [name]: modValue
-    //         }
-    
-    //       }
-    //     }
-    //     , () => {
-    
-    //       this.props.loadItems(this.state.filterBy);
-    //     })
-    
-    //   }
     render() {
         const { loggedInUser } = this.props;
-       
+
         return <React.Fragment>
-            <header>
-                <nav className="main-header">
-                    <NavLink exact to="/store"><div className="logo"><span className="pic">Pic</span><span>&</span><span className="art">Art</span></div></NavLink>
-                        <Button className="btn-hamburger"><img src={Menu} alt="menu"/></Button>
-                        {/* <ItemFilter handleInput={this.handleInput} /> */}
-                    <div className="nav-bar">
+            <header className="header">
+            <nav className="main-header">
+            <NavLink exact to="/store"><div className="logo"><span className="pic">Pic</span><span>&</span><span className="art">Art</span></div></NavLink>
+  <input class="menu-btn" type="checkbox" id="menu-btn" />
+  <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+  <div class="menu">
                         <NavLink to="/login">Login</NavLink>
-                        <NavLink to="/chat">About Us</NavLink>
+                        <NavLink to="/about">About Us</NavLink>
                         {loggedInUser && <span className="loggedin-user">
                             <Link to={`/user/${loggedInUser._id}`}>
-                             Welcome, {loggedInUser.fullname}
+                                Welcome, {loggedInUser.fullname}
                             </Link>
                         </span>}
                     </div>
-                    {/* <Cart /> */}
                 </nav>
             </header>
         </React.Fragment>
